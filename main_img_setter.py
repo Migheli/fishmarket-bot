a4e7250b1d6e204c478bfcc6b3c4404b80f9878b
Migheli <98918370+Migheli@users.noreply.github.com>
2022-06-06 23:38:47 +0300

    Changed img_setter.py

--- main_img_setter.py
+++ main_img_setter.py
@@ -5,8 +5,6 @@ from api_handlers import get_auth_token, get_product_catalogue
 
 logger = logging.getLogger(__name__)
 
-MOLTIN_TOKEN = get_auth_token()
-
 
 def upload_img_and_get_id(moltin_token, url):
     headers = {'Authorization': f'Bearer {moltin_token}'}
@@ -22,8 +20,7 @@ def upload_img_and_get_id(moltin_token, url):
     return response.json()['data']['id']
 
 
-def get_product_id_by_name(product_name):
-    products = get_product_catalogue(MOLTIN_TOKEN)['data']
+def get_product_id_by_name(products, product_name):
     for product in products:
         if product['name'] == product_name:
             return product['id']
@@ -45,17 +42,15 @@ def set_main_image_to_product(moltin_token, product_id, file_id):
     response.raise_for_status()
 
 
-def set_main_image_by_product_name(url, product_name):
-    file_id = upload_img_and_get_id(MOLTIN_TOKEN, url)
-    product_id = get_product_id_by_name(product_name)
-    set_main_image_to_product(MOLTIN_TOKEN, product_id, file_id)
-
-
 def main():
     try:
+        moltin_token = get_auth_token()
         product_name = os.getenv('PRODUCT_NAME')
         img_url = os.getenv('IMG_URL')
-        set_main_image_by_product_name(img_url, product_name)
+        file_id = upload_img_and_get_id(moltin_token, img_url)
+        products = get_product_catalogue(moltin_token)['data']
+        product_id = get_product_id_by_name(products, product_name)
+        set_main_image_to_product(moltin_token, product_id, file_id)
     except Exception as err:
         logging.error('Ошибка в привязке фото к товару:')
         logging.exception(err)
